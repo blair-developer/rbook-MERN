f36da61e34e0bf2b42a5895c5a396e790da341b0
Tonny-1999 <oduortonny10@gmail.com>
2024-01-20 15:09:21 +0300

    first commit and project

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+try {
+    await mongoose.connect(process.env.MONGO);
+    console.log('connected to MongoDB');
+  } catch (error) {
+    throw error
+  } 
+};
+
+mongoose.connection.on("disconnected", ()=>{
+    console.log("MongoDB disconected");
+});
+mongoose.connection.on("connected", ()=>{
+    console.log("MongoDB conected");
+});
+
+app.listen(8800, () =>{
+    connect()
+    console.log("connected to backed")
+})
